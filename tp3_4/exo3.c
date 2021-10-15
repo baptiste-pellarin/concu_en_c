85c0c35a821900ba65798952c51c2c7d0a063b9f
Baptiste PELLARIN <baptiste.pellarin@cpe.fr>
2021-10-15 19:18:03 +0200

    Exo3 pipes

--- tp3_4/exo3.c
+++ tp3_4/exo3.c
@@ -1,73 +1,144 @@
+/*
+	Baptiste PELLARIN - Julien Gautron (2021/2022)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> 
-#include <string.h>
-#include <fcntl.h>
-#include <time.h>
+#include <sys/wait.h>
 
+#define NB_RAND 5
 
 int main() {
 
-	srand(time(NULL));
-	
-	pid_t filtreImpaire, filtrePaire, generateur;
-	
-	int pipeImpaire[2], pipePaire[2], randint;
-	
-	int nombresGeneres[100];
-	
+	srand(getpid());
+		
+	int pipeImpaire[2], pipePaire[2], sommePaire[2], sommeImpaire[2], randint;
+		
 	pipe(pipeImpaire);
 	pipe(pipePaire);
-	
-	close(pipePaire[0]);
-	close(pipeImpaire[0]);
-	for(int i =0; i< 100; i++) {
-		
-		
-		randint = rand()%100;
-		printf("%i\n", randint);
-		if (randint % 2){
-			write(pipePaire[1], randint, sizeof(int));
-		} else {
-			write(pipeImpaire[1], randint, sizeof(int));
-		}
-		  
-	}
-	
-	close(pipeImpaire[1]);
-	close(pipePaire[1]);
+	pipe(sommeImpaire);
+	pipe(sommePaire);
 
+	if(!fork()){
+
+		printf("Générateur\n");
 	
-	/*
-	
-	pid_t generateur = fork();
-	
-	if (generateur == 0){
-		
-		int randint = rand();
-		
-		if (randint % 2 == 0){
-			close(pipePaire[0]);
-			write(pipePaire[1], randint, strlen(randint));
-			close(pipePaire[1]);
-		} else {
-			close(pipeImpaire[0]);
-			write(pipeImpaire[1], randint, strlen(randint));
-			close(pipeImpaire[1]);
+		close(pipePaire[0]);
+		close(pipeImpaire[0]);
+		close(sommeImpaire[1]);
+		close(sommePaire[1]);
+
+		for(int i =0; i< NB_RAND; i++) {
+			
+			
+			randint = rand()%100;
+
+			//printf("Gen %i\n", randint);
+			if (randint % 2 == 0){
+				write(pipePaire[1], &randint, sizeof(int));
+			} else {
+				write(pipeImpaire[1], &randint, sizeof(int));
+			}
+			
 		}
+
+		randint = -1;
+
+		write(pipeImpaire[1], &randint, sizeof(int));
+
+		write(pipePaire[1], &randint, sizeof(int));
+
+		int somme_totale = 0, somme_impaire = 0, somme_paire = 0;	
+
+		read(sommeImpaire[0], &somme_impaire, sizeof(int));
+		read(sommePaire[0], &somme_paire, sizeof(int));
+
+		somme_totale = somme_impaire + somme_paire;
+
+		printf("\n-------\nTotal Paire : %i\nTotal Impaire : %i\nTotal : %i\n", somme_paire, somme_impaire, somme_totale);
+
 		
+		close(sommeImpaire[0]);
+		close(sommePaire[0]);
+		close(pipeImpaire[1]);
+		close(pipePaire[1]);
+		exit(0);
+	}
+
+	if(!fork()){
+		close(pipeImpaire[1]);
+		close(pipeImpaire[0]);
+		close(sommeImpaire[0]);
+		close(sommeImpaire[1]);
+		close(sommePaire[0]);
+		close(pipePaire[1]);
+
+
+		int nombre = 0, somme=0;
+		//printf("Filtre paire\n");
+
+		while(nombre != -1){
+
+			read(pipePaire[0], &nombre, sizeof(int));
+			if(nombre >= 0){
+				printf("%d paire\n", nombre);
+				somme = somme + nombre;
+			} else {
+				printf("Fin pipe\n");
+			}
+			//printf("Somme paire %d\n", somme);
+		}
+
+		if(write(sommePaire[1], &somme, sizeof(int)) == -1){
+			perror("Erreur ecriture dans le pipe");
+		}
+
+		close(pipePaire[0]);
+		close(sommePaire[1]);
+		exit(0);
+
 	}
 	
-	pid_t filtreImpaire = fork();
-	
-	if (filtreImpaire == 0) {
-	
+
+
+
+	if(!fork()){
+		close(pipeImpaire[1]);
+		close(sommeImpaire[0]);
+		close(sommePaire[1]);
+		close(sommePaire[0]);
+		close(pipePaire[0]);
+		close(pipePaire[1]);
+
+		int nombre = 0, somme = 0;
+		//printf("Filtre impaire\n");
+
+		while(nombre != -1){
+
+			read(pipeImpaire[0], &nombre, sizeof(int));
+			if(nombre >= 0){
+				printf("%d impaire\n", nombre);
+				somme = somme + nombre;
+			} else {
+				printf("Fin pipe\n");
+			}
+			//printf("Somme impaire %d\n", somme);
+		}
+		if(write(sommeImpaire[1], &somme, sizeof(int)) == -1){
+			perror("Erreur ecriture dans le pipe");
+		}
+
+
+		close(pipeImpaire[0]);
+		close(sommeImpaire[1]);
+
+		exit(0);
+
 	}
-	
-	*/
-	
-	
-	
+
+	while(wait(NULL) != -1);
+
 	
 	
 	return 0;
